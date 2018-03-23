01ddfee816a2e3e1f032a4c3ec7cb92b6f54c33a
Luis <lfr4704@gmail.com>
2018-03-23 12:16:33 -0700

    revised

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$( document ).ready( function () {
+
+$('#main-menu a').click(function(event) {
+   event.preventDefault();
+ 
+   $.ajax(this.href, {
+      success: function(data) {
+         $('#main').html($(data).find('#main *'));
+         $('#notification-bar').text('The page has been successfully loaded');
+      },
+      error: function() {
+         $('#notification-bar').text('An error occurred');
+      }
+   });
+});
+
+});
